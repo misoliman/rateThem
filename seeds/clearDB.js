147606074e6106226d15d93842cab75cc6a1453d
Mohamed Ibrahim Mohamed Osman <mimosman@tamgo.com>
2024-07-13 13:09:46 +0300

    clear altasDB, try solving timeout error vercel

---
+++ seeds/clearDB.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Restaurant = require("../models/restaurants");
+const Review = require("../models/review");
+const User = require("../models/users");
+
+const altalsDB = "mongodb://user123:user123@ac-zmjq9rq-shard-00-00.fgtkwo0.mongodb.net:27017,ac-zmjq9rq-shard-00-01.fgtkwo0.mongodb.net:27017,ac-zmjq9rq-shard-00-02.fgtkwo0.mongodb.net:27017/?ssl=true&replicaSet=atlas-ohe4g3-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster4";
+const localDB = "mongodb://localhost:27017/rate-them";
+
+mongoose.connect(altalsDB)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+const clearDB = async () => {
+    try {
+        await Restaurant.deleteMany({});
+        await User.deleteMany({})
+        await Review.deleteMany({})
+
+        console.log('Database cleared successfully');
+    } catch (error) {
+        console.error('Error seeding the database:', error);
+    } finally {
+        mongoose.connection.close();
+    }
+};
+
+clearDB();
